ef9927a842f6008e6cc244ddf4149e59f2cdee72
sverdi <46566997+sverdi@users.noreply.github.com>
2019-05-16 08:19:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ let render = Render.create({
 });
 Engine.run(engine);
 Render.run(render);
- 
+
 let ball=Bodies.circle(200, 10, 40);
 let floor=Bodies.trapezoid(340,300,500,100,.9,{isStatic: true});
 let myCar=Composites.car(390, 0, 100, 30, 40);
 let myCradle=Composites.newtonsCradle(600, 100, 5, 10, 160);
 World.add(engine.world, [ball,floor,myCar,myCradle]);
- 
+
 let world = engine.world;
 let Mouse= Matter.Mouse;
 let MouseConstraint=Matter.MouseConstraint;
